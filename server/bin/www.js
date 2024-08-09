cfe56116e1c204006272db96f2fe886b7d61cb07
ImamAsyariAlmaturidi <imamasyari700@gmail.com>
2024-08-09 12:14:55 +0700

    deployment

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,5 +1,5 @@
 const app = require('../app')
-const port = process.env_PORT || 3000
+const port = process.env_PORT || 80
 
 app.listen(port, () => {
     console.log(`server running in port ${port} `)
